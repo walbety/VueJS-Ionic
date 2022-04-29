437d4433198cbdc1f4940828e342867d8a3b2862
walbetyaraujo-mbciet <walbety.araujo@ext.mercadobitcoin.com.br>
2022-04-29 18:24:14 -0300

    Created base project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # VueJS-Vite
 Basic VueJS app with Vite
+
+# first time commands
+npm install
+
+# to run the app
+npm run dev
---
+++ src/components/MyForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <input type="text" placeholder="Name" v-model="name" />
+    <div>
+      <button @click="submitForm" v-bind:disabled="name.length == 0">
+        Submit
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const name = ref("");
+
+    const submitForm = () => {
+      console.log(`form submited with name: ${name.value}`);
+    };
+
+    return {
+      name,
+      submitForm,
+    };
+  },
+};
+</script>
+
+<style>
+input[type="text"] {
+  padding: 5px;
+}
+
+button {
+  margin-top: 20px;
+}
+</style>
